ba92b689264ce66f9671e13df171db4732243fec
rahul7011 <champrahul01@gmail.com>
2019-08-14 00:07:18 +0530

    added blog models to the admin panel.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class blog(models.Model):
+    image = models.ImageField(upload_to='images/')
+    body = models.TextField()
+    pub_date = models.DateTimeField()
+    title = models.CharField(max_length=225)
