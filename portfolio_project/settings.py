6804b9b7b2535da63f5e3bbb8bbf8d52ffb74e4b
rahul7011 <champrahul01@gmail.com>
2019-08-13 23:51:06 +0530

    changed the database to POSTGRES and added portfoliodb(database) to it.

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'portfolio_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER':'postgres',
+        'PASSWORD':'superuser1234',
+        'HOST':'localhost',
     }
 }
 
