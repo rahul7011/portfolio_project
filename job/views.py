9a8ba26911cdc8c3c6d4cc38bd311340152ab59d
rahul7011 <champrahul01@gmail.com>
2019-08-14 16:08:54 +0530

    Added bootstrap and jobs models to the main page.

--- job/views.py
+++ job/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import jobs
 
 # Create your views here.
+
+def home(request):
+    job = jobs.objects.all()
+    
+    return render(request,'job/home.html',{'jobs':job})
